040246c516ff20887b1b14a44684fcb4ee48d487
Chili <vasapofficial@gmail.com>
2024-05-31 20:29:41 +0200

    feat: add functions to add and get foods

--- controllers/VendorController.ts
+++ controllers/VendorController.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 import { EditVendorInputs, VendorLoginInputs } from '../dto'
 import { FindVendor } from './AdminController'
 import { GenerateSignature, ValidatePassword } from '../utils/PasswordUtil'
+import { CreateFoodInputs } from '../dto/Food.dot'
+import { Food } from '../models'
 
 export const VendorLogin = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = <VendorLoginInputs>req.body
@@ -67,6 +69,29 @@ export const UpdateVendorProfile = async (req: Request, res: Response, next: Nex
   return res.json({ 'message': 'Existing Vendor not found'})
 }
 
+export const UpdateVendorCoverImage = async (req: Request,res: Response, next: NextFunction) => {
+  const user = req.user;
+
+   if(user){
+     const vendor = await FindVendor(user._id);
+
+     if(vendor !== null){
+      const files = req.files as [Express.Multer.File];
+
+      const images = files.map((file: Express.Multer.File) => file.filename);
+
+      vendor.coverImages.push(...images);
+
+      const saveResult = await vendor.save();
+          
+      return res.json(saveResult);
+     }
+
+  }
+  
+  return res.json({'message': 'Unable to Update vendor profile '})
+}
+
 export const UpdateVendorService = async (req: Request, res: Response, next: NextFunction) => {
   const user = req.user
 
@@ -83,4 +108,53 @@ export const UpdateVendorService = async (req: Request, res: Response, next: Nex
   }
 
   return res.json({ 'message': 'Existing Vendor not found'})
+}
+
+export const AddFood = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.user
+
+  if(user) {
+    const { name, description, category, foodType, readyTime, price } = <CreateFoodInputs>req.body
+
+    const vendor = await FindVendor(user._id)
+
+    if(vendor !== null) {
+
+      const files = req.files as [Express.Multer.File]
+
+      const images = files.map((file: Express.Multer.File) => file.filename)
+      const createFood = await Food.create({
+        vendorId: vendor.id,
+        name: name,
+        description: description,
+        category: category,
+        foodType: foodType,
+        images: images,
+        readyTime: readyTime,
+        price: price,
+        rating: 0
+      })
+
+      vendor.foods.push(createFood)
+      const result = await vendor.save();
+
+      return res.json(result)
+    }
+  }
+
+  return res.json({ 'message': 'Something went wrong with add food'})
+}
+
+export const GetFood = async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.user
+
+  if(user) {
+    const foods = await Food.find({ vendorId: user._id})
+
+    if(foods !== null) {
+      return res.json(foods)
+    }
+  }
+
+  return res.json({ 'message': 'Foods information not found'})
 }
--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,15 +1,31 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { GetVendorProfile, UpdateVendorProfile, VendorLogin, UpdateVendorService } from '../controllers'
+import { GetVendorProfile, UpdateVendorProfile, VendorLogin, UpdateVendorService, AddFood, GetFood, UpdateVendorCoverImage } from '../controllers'
 import { Authenticate } from '../middlewares'
+import multer from 'multer'
 
 const router = express.Router()
 
 router.post('/login', VendorLogin)
 
+const imageStorage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'images')
+  },
+  filename: function(req,file,cb){
+    cb(null, new Date().toISOString().replace(/:/g, "-") + "_" + file.originalname);
+  }
+})
+
+const images = multer({ storage: imageStorage}).array('images', 10);
+
 router.use(Authenticate)
 router.get('/profile', GetVendorProfile)
 router.patch('/profile', UpdateVendorProfile)
+router.patch('/coverimage', images, UpdateVendorCoverImage)
 router.patch('/service', UpdateVendorService)
+
+router.post('/food', images, AddFood)
+router.get('/foods', GetFood)
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({ message: 'hello from Vendor'})
